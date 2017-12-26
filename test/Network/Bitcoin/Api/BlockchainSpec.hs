5e7453a72dc8c9c794b19d8f9bc4e1bb456c7bdc
sahabi <sahabi@gmail.com>
2017-12-26 15:41:07 -0600

    removed all tests except for the blockchain where I added more tests

--- test/Network/Bitcoin/Api/BlockchainSpec.hs
+++ test/Network/Bitcoin/Api/BlockchainSpec.hs
@@ -1,27 +1,33 @@
 module Network.Bitcoin.Api.BlockchainSpec where
 
-import qualified Network.Bitcoin.Api.Blockchain               as Blockchain
+import qualified Network.Bitcoin.Api.Blockchain     as Blockchain
 import           Network.Bitcoin.Api.TestUtil (testClient)
 import           Test.Hspec
+import           Data.Char
+import           Data.HexString
+import           Data.Text    (unpack)
 
 spec :: Spec
 spec = do
   describe "when testing blockchain functions" $ do
-   it "can request blockcount" $ do
-     r <- testClient Blockchain.getBlockCount
+      it "can request blockcount" $ do
+        r <- testClient Blockchain.getBlockCount
+        r `shouldSatisfy` (>= 100)
 
-     r `shouldSatisfy` (>= 100)
+      it "can request the latest block" $ do
+        r <- testClient Blockchain.getBestBlockHash
+        (unpack $ toText r) `shouldStartWith` ['0']
 
-   it "can request block hashes" $ do
-     testClient $ \client -> do
-       count  <- Blockchain.getBlockCount client
-       hashes <- mapM (Blockchain.getBlockHash client) [0..count - 1]
 
-       fromIntegral (length (hashes)) `shouldBe` count
+      it "can request a block based on its hash" $ do
+        hash <- testClient $ \client -> do
+          (Blockchain.getBlockHash client) 1
+        block <- testClient $ \client -> do
+          Blockchain.getBlock client hash
+        1 `shouldSatisfy` (== 1)
 
-   it "can request blocks" $ do
-     testClient $ \client -> do
-       count  <- Blockchain.getBlockCount client
-       blocks <- mapM (Blockchain.getBlock client) =<< mapM (Blockchain.getBlockHash client) [0..count - 1]
-
-       fromIntegral (length (blocks)) `shouldBe` count
+      it "can request blocks" $ do
+         testClient $ \client -> do
+           hashes <- mapM (Blockchain.getBlockHash client) [0..10]
+           blocks <- mapM (Blockchain.getBlock client) hashes
+           fromIntegral (length (blocks)) `shouldBe` 11
--- test/Network/Bitcoin/Api/TestUtil.hs
+++ test/Network/Bitcoin/Api/TestUtil.hs
@@ -1,15 +1,10 @@
-module Network.Bitcoin.Api.TestUtil (testClient, isStatusCodeException) where
+module Network.Bitcoin.Api.TestUtil (testClient) where
 
 import qualified Data.Text                                    as T (pack)
 import           Control.Lens                                 ((^.))
-import           Network.HTTP.Client                          (HttpException (..))
 import           Network.Wreq.Lens                            (statusCode)
 
 import           Network.Bitcoin.Api.Client
 
 testClient :: (Client -> IO a) -> IO a
-testClient = withClient "127.0.0.1" 18332 (T.pack "user") (T.pack "pass")
-
-isStatusCodeException :: Int -> HttpException -> Bool
-isStatusCodeException code (StatusCodeException s _ _) = s ^. statusCode == code
-isStatusCodeException _ _ = False
+testClient = withClient "127.0.0.1" 8332 (T.pack "sahabi") (T.pack "5555")
