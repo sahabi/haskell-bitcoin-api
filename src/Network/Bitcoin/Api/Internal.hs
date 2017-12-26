ad632bc06bf88dacc6da4e50dd7307d0bdc2aac0
sahabi <sahabi@gmail.com>
2017-12-26 01:58:29 -0600

    removed offending lines

--- src/Network/Bitcoin/Api/Internal.hs
+++ src/Network/Bitcoin/Api/Internal.hs
@@ -89,10 +89,6 @@ call client method params =
                 command
         case rE of
           Right r -> return (r ^. W.responseBody)
-          Left ex@(StatusCodeException _ hdrL _) ->
-            throwNothing (hdrM >>= decode . BL.fromStrict)
-                where hdrM = lookup "X-Response-Body-Start" hdrL
-                      throwNothing = maybe (E.throwM (ex :: HttpException)) return
           Left ex -> E.throwM (ex :: HttpException)
 
   in do
