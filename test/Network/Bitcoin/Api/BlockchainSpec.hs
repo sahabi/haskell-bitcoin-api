91b0cae0aba53adc8cc9f62883dd79695913150f
sahabi <sahabi@gmail.com>
2017-12-26 20:18:14 -0600

    added more tests: getChainTips

--- test/Network/Bitcoin/Api/BlockchainSpec.hs
+++ test/Network/Bitcoin/Api/BlockchainSpec.hs
@@ -1,9 +1,11 @@
 module Network.Bitcoin.Api.BlockchainSpec where
 
+import           Prelude                         as P
 import           Control.Lens
-import qualified Network.Bitcoin.Api.Blockchain     as Blockchain
+import qualified Network.Bitcoin.Api.Blockchain     as BC
 import           Network.Bitcoin.Api.TestUtil (testClient)
 import           Network.Bitcoin.Api.Types.BlockChainInfo
+import           Network.Bitcoin.Api.Types.ChainTips as CT
 import           Test.Hspec
 import           Data.Bitcoin.Block  (blockHeader, merkleRoot)
 import           Data.HexString
@@ -16,12 +18,12 @@ spec = do
   describe "when testing blockchain functions" $ do
 
       it "can request the latest block" $ do
-        r <- testClient Blockchain.getBestBlockHash
+        r <- testClient BC.getBestBlockHash
         (T.unpack $ toText r) `shouldStartWith` ['0']
 
       it "can request a block based on its hash (main)" $ do
         r <- testClient $ \client -> do
-          Blockchain.getBlock client $ hexString $ BS.pack "00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048"
+          BC.getBlock client $ hexString $ BS.pack "00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048"
         o <- case r of
                Just x -> return (x^.blockHeader.merkleRoot)
                Nothing -> return(0)
@@ -29,10 +31,10 @@ spec = do
 
       it "can request a block based on its hash (main)" $ do
         h <- testClient $ \client -> do
-          (Blockchain.getBlockHash client) 0
+          (BC.getBlockHash client) 0
         r <- testClient $ \client -> do
-          Blockchain.getBlock client h
-        Prelude.putStrLn $ show r
+          BC.getBlock client h
+        P.putStrLn $ show r
         o <- case r of
                Just x -> return (x^.blockHeader.merkleRoot)
                Nothing -> return(0)
@@ -40,20 +42,33 @@ spec = do
 
       it "can request multiple blocks successfully" $ do
         testClient $ \client -> do
-          hs <- mapM (Blockchain.getBlockHash client) [0..10]
-          bs <- mapM (Blockchain.getBlock client) hs
-          fromIntegral (Prelude.length (bs)) `shouldBe` 11
+          hs <- mapM (BC.getBlockHash client) [0..10]
+          bs <- mapM (BC.getBlock client) hs
+          fromIntegral (P.length (bs)) `shouldBe` 11
 
       it "can request info: the current state of the chain (main)" $ do
-        r <- testClient Blockchain.getBlockChainInfo
+        r <- testClient BC.getBlockChainInfo
         (T.unpack $ chain r) `shouldSatisfy` (== "main")
 
       it "can request blockcount" $ do
-        r <- testClient Blockchain.getBlockCount
+        r <- testClient BC.getBlockCount
         r `shouldSatisfy` (>= 100)
 
       it "can request the block hash from height" $ do
         r <- testClient $ \client -> do
-          (Blockchain.getBlockHash client) 1
+          (BC.getBlockHash client) 1
         r `shouldSatisfy`  (== (hexString $ BS.pack "00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048"))
 
+      it "can request the block header from the block hash" $ do
+        h <- testClient $ \client -> do
+          (BC.getBlockHash client) 0
+        r <- testClient $ \client -> do
+          (BC.getBlockHeader client) h
+        case r of
+          Just x -> x `shouldSatisfy` (== (hexString $ BS.pack "0100000000000000000000000000000000000000000000000000000000000000000000003ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a29ab5f49ffff001d1dac2b7c"))
+          Nothing -> error "we got nothing"
+
+
+      it "can request info: hgiest-height block of each local block chain" $ do
+        r <- testClient BC.getChainTips
+        (height $ P.head r) `shouldSatisfy` (> 500000)
